0f3f3548e60609b0d86af1cf83cdb7094d860f91
nesnip <hpmarines12@gmail.com>
2020-10-17 13:17:06 -0500

    add login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
+import logoMejorado from '../assets/images/logo-mejorado.png';
 import '../css/Header.css';
 
 const Header = () => {
     return (
-        <nav class="header navbar navbar-expand-lg navbar-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse btn-container" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="font-color" href="#">Crear cuenta<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <span class="font-color">|</span>
-                </li>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#"><img src={logoMejorado} className="logo-bbva" alt="BBVA"/></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="font-color" href="#">Ingresar</a>
+                    <a class="nav-link" href="#">Crear cuenta</a>
                 </li>
-            </ul>
-        </div>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Ingresar <span class="sr-only">(current)</span></a>
+                </li>
+                </ul>
+            </div>
         </nav>
     )
 };
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import '../css/Login.css';
 
 const Login = () => {
@@ -19,30 +20,34 @@ const Login = () => {
         <Header />
         <section className="form-container">
             <form className="login-form">
-                <h3>Inicia sesión</h3>
+                <h3 className="login-title">Ñawiriy</h3>
                 <div>
-                <input
-                    type="email" 
-                    placeholder="Ingresar DNI"
-                    value={dni}
-                    onChange={handleDni}
-                    className="form-control"
-                />
+                    <input
+                        type="email" 
+                        placeholder="Ingresar DNI"
+                        value={dni}
+                        onChange={handleDni}
+                        className="form-control login-input"
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Ingresar contraseña"
+                        value={password}
+                        onChange={handlePassword}
+                        className="form-control"
+                    />
                 </div>
-                <div>
-                <input 
-                    type="password" 
-                    placeholder="Ingresar contraseña"
-                    value={password}
-                    onChange={handlePassword}
-                    className="form-control"
-                />
+                <div class="form-group form-check login-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+                    <label class="form-check-label" for="exampleCheck1">Recordar documento</label>
                 </div>
-                <p><a href="/#">¿Olvidaste tu contraseña?</a></p>
-                <button type="submit" class="btn btn-primary">Cancelar</button>
-                <button type="submit" class="btn btn-primary">Ingresar</button>
+                <div className="">
+                    <button type="submit" class="btn btn-primary login-btn">Cancelar</button>
+                    <button type="submit" class="btn btn-primary login-btn">Ingresar</button>
+                </div>
             </form>
         </section>
+        <Footer />
       </>
     )
   };
